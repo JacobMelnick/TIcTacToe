6907d52f7a55f2b99236c25f78cb2d2fe143f334
Jacob Melnick <jake.r.emelnick@gmail.com>
2021-05-24 11:00:15 -0700

    Created reset button and playability

--- app.js
+++ app.js
@@ -20,29 +20,95 @@ const cells = [
     cell9
 ]
 
-let player1 = true
-let player2 = false
+let players = 0
+
+const choices = {
+    x: 'X',
+    o: 'O'
+}
+//check for winning situations
+const findWinner = (player) => {
+
+    if (cell1.textContent === player && cell5.textContent === player && cell9 === player) {
+        return 
+    }
+    if (cell3.textContent === player && cell5.textContent === player && cell7 === player) {
+        return
+    }
+    if (cell1.textContent === player && cell2.textContent === player && cell3 === player) {
+        return
+    }
+    if (cell4.textContent === player && cell5.textContent === player && cell6 === player) {
+        return
+    }
+    if (cell7.textContent === player && cell8.textContent === player && cell9 === player) {
+        return
+    }
+    if (cell1.textContent === player && cell4.textContent === player && cell7 === player) {
+        return
+    }
+    if (cell2.textContent === player && cell5.textContent === player && cell8 === player) {
+        return
+    }
+    if (cell3.textContent === player && cell6.textContent === player && cell9 === player) {
+        return
+    }
+
+}
+console.log(findWinner)
+
+const handleClick = (e) => {
+const currentSquare = e.target
+//if the current spot is taken dont go to the next turn
+if (currentSquare.textContent === choices.x || currentSquare.o) {
+    return
+}
+// if someone has won then return
+if (findWinner(choices.x) === true || findWinner(choices.o) === true) {
+    return;
+}
+
+// handle turn
+if (players % 2 === 0) {
+    currentSquare.textContent = choices.x 
+} else {
+    currentSquare.textContent = choices.o
+}
+
+//handle winners
+
+if (findWinner(choices.x)) {
+    alert(`The Player ${choices.x} has won`)
+} 
+if (findWinner(choices.o)) {
+    alert(`The Player ${choices.o} has won`)
+}
+
+// increment player by one to go to next players turn
+players++
+
+}
+
+// assign a click listerner to each cell
+
+const assignClick = (cell) => {
+    cell.addEventListener('click', handleClick)
+}
+
+// assign that click to each cell
+cells.forEach(assignClick)
 
 
-const firstMove = cells.map((spot, i) => {
-    spot.addEventListener('click', (e) => {
-        if (player1 === true) {
-            spot.innerHTML = 'X'
-            if (spot.innerHTML === 'X') {
-                player1 = false
-                player2 = true
-            }
-        }
-    })
+// reset the game
+
+const resetSquares = () => {
+cells.map(cell => {
+    cell.textContent = ""
 })
-const secondMove = cells.map((spot, i) => {
-    spot.addEventListener('click', (e) => {
-        if (player2 === true) {
-            spot.innerHTML = '0'
-            if (spot.innerHTML === '0') {
-                player1 = true
-                player2 = false
-            }
-        }
-    })
+}
+
+document.getElementById('reset').addEventListener('click', () => {
+    players = 0
+    resetSquares()
 })
+
